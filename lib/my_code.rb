d9d0c9138a7c88cae096713d4fecf5026fe6cdff
Andrius Jurgsaitis <andriusj6@hotmail.com>
2019-08-21 21:43:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,7 +10,7 @@ def map(source_array)
   new_array
 end
 
-def reduce(source_array, starting_point = 100)
+def reduce(source_array, starting_point = 0)
     total = starting_point
     counter = 0
   

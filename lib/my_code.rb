b422f29e9b7e4b31912253ea512b10c411f7839a
Andrius Jurgsaitis <andriusj6@hotmail.com>
2019-08-21 21:31:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,12 @@
 # Your Code Here
+
+def map(source_array)
+  new_array = []
+
+    source_array.length.times { |index|
+    new_array.push(yield(source_array[index]))
+    }
+    
+  new_array
+end
+
